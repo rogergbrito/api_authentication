1a257825efd495f8e5b700c19fdc27e1b93d9a89
roger <roogerbrito1@gmail.com>
2023-11-02 01:00:32 -0300

    adicionado cors no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import './database/connect';
 import router from './routes/router';
 import rateLimit from 'express-rate-limit';
 
 const app: Application = express();
-
 const port = process.env.PORT || 3001;
+
+const allowedUrls = ['http://localhost:5000', 'http://localhost:5500'];
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
@@ -13,6 +15,18 @@ const limiter = rateLimit({
   message: 'Too many accounts created from this IP, please try again after an hour',
   legacyHeaders: false,
 })
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin || allowedUrls.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    };
+  },
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
 app.use(express.json());
 app.use(limiter);
