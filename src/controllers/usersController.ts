0c8f176c1e4d3190454285e7aad699d9d605286f
roger <roogerbrito1@gmail.com>
2023-11-04 12:16:49 -0300

    alterada tabela role para admin do tipo boolean e docker-compose atualizado

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -6,10 +6,10 @@ export async function getAllUsers(req: Request, res: Response) {
 
   try {
     const query = `
-    SELECT id, name, email, active, role
+    SELECT id, name, email, active, admin, created_at
     FROM users
     ORDER BY id ASC;
-    `
+    `;
 
     const result = await pool.query(query);
     if (result.rowCount === 0) {
@@ -29,9 +29,9 @@ export async function getUserById(req: Request, res: Response) {
 
   try {
     const query = `
-    SELECT id, name, email, active, role
+    SELECT id, name, email, active, admin
     FROM users
-    WHERE id = $1;`
+    WHERE id = $1;`;
 
     const result = await pool.query(query, [userId]);
 
@@ -54,21 +54,21 @@ export async function updateUserPut(req: Request, res: Response) {
       name: req.body.name,
       email: req.body.email,
       active: req.body.active,
-      role: req.body.role
+      admin: req.body.admin
     };
 
-    if (!user.name || !user.email || !user.role) return res.status(400).json({ error: "User not sent correctly" });
+    if (!user.name || !user.email || !user.admin) return res.status(400).json({ error: "User not sent correctly" });
 
     if (user.active !== true && user.active !== false) return res.status(400).json({ error: "Active parameter must be boolean" });
-    if (user.role !== 'admin' && user.role !== 'regular') return res.status(400).json({ error: "Role parameter must be admin or regular" });
+    if (user.admin !== true && user.admin !== false) return res.status(400).json({ error: "Admin parameter must be boolean" });
 
     const query = `
     UPDATE users 
-    SET name = $1, email = $2, active = $3, role = $4
+    SET name = $1, email = $2, active = $3, admin = $4
     WHERE id = $5;
     `;
 
-    const result = await pool.query(query, [user.name, user.email, user.active, user.role, id]);
+    const result = await pool.query(query, [user.name, user.email, user.active, user.admin, id]);
 
     if (result.rowCount === 0) return res.status(400).json({ error: "Error updating user" });
 
@@ -88,13 +88,13 @@ export async function updateUserPatch(req: Request, res: Response) {
       name: req.body.name,
       email: req.body.email,
       active: req.body.active,
-      role: req.body.role
+      admin: req.body.admin
     };
 
-    if (!user.name && !user.email && !user.role && user.active === undefined) return res.status(400).json({ error: "User not sent correctly" });
+    if (!user.name && !user.email && !user.admin && (user.active === undefined)) return res.status(400).json({ error: "User not sent correctly" });
 
     if (user.active !== true && user.active !== false && user.active !== undefined ) return res.status(400).json({ error: "Active parameter must be boolean" });
-    if (user.role !== 'admin' && user.role !== 'regular' && user.role !== undefined) return res.status(400).json({ error: "Role parameter must be admin or regular" });
+    if (user.admin !== true && user.admin !== false && user.admin !== undefined) return res.status(400).json({ error: "Admin parameter must be boolean" });
 
     const querySelect = `
     SELECT *
@@ -108,16 +108,16 @@ export async function updateUserPatch(req: Request, res: Response) {
 
     const queryUpdate = `
     UPDATE users
-    SET name = $1, email = $2, active = $3, role = $4
+    SET name = $1, email = $2, active = $3, admin = $4
     WHERE id = $5;
     `;
 
     if (!user.name) user.name = databaseUser.rows[0].name;
     if (!user.email) user.email = databaseUser.rows[0].email;
     if (user.active !== false && user.active !== true) user.active = databaseUser.rows[0].active;
-    if (!user.role) user.role = databaseUser.rows[0].role;
+    if (!user.admin) user.admin = databaseUser.rows[0].admin;
 
-    const result = await pool.query(queryUpdate, [user.name, user.email, user.active, user.role, id]);
+    const result = await pool.query(queryUpdate, [user.name, user.email, user.active, user.admin, id]);
 
     if (result.rowCount === 0) return res.status(404).json({ error: "Error updating user" });
 
